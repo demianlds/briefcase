b2d5d7b7b0b8b2a360fd8e88b0fd337f81efe177
demianlds <demian.igi@gmail.com>
2024-05-09 12:12:30 -0300

    restructuring part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'briefcase';
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    if (this.router.url === '/') {
+      this.router.navigate(['/portafolio']);
+    }
+  }
+
+  isActive(url: string): boolean {
+    return this.router.url.includes(url);
+  }
 }
