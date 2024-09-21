6929d09444449d0b698c7a4340949be8d8e6892f
demianlds <demian.igi@gmail.com>
2024-09-21 04:23:12 -0300

    Add responsives styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,14 +6,41 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'briefcase';
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private elRef: ElementRef, // Inyección de dependencia para acceder al DOM
+    private renderer: Renderer2 // Para manipular el DOM de manera segura
+  ) { }
 
   ngOnInit(): void {
     if (this.router.url === '/') {
       this.router.navigate(['/portafolio']);
+    }
+  }
+
+  ngAfterViewInit(): void {
+    // Accede al botón y al menú de navegación
+    const toggler = this.elRef.nativeElement.querySelector('.navbar-toggler');
+    const navbarCollapse = this.elRef.nativeElement.querySelector('#navbarNav');
+
+    if (toggler && navbarCollapse) {
+      // Escucha el evento click en el botón y alterna la clase 'active'
+      this.renderer.listen(toggler, 'click', () => {
+        const isExpanded = navbarCollapse.classList.contains('active');
+        
+        if (isExpanded) {
+          // Si está abierto, lo cierra
+          this.renderer.removeClass(navbarCollapse, 'active');
+          toggler.setAttribute('aria-expanded', 'false');
+        } else {
+          // Si está cerrado, lo abre
+          this.renderer.addClass(navbarCollapse, 'active');
+          toggler.setAttribute('aria-expanded', 'true');
+        }
+      });
     }
   }
 
